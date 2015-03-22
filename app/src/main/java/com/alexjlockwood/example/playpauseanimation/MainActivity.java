4371d2cbbfb65d9ac86ecf384fb245e562853258
Alex Lockwood <alexjlockwood@gmail.com>
2015-03-21 22:50:51 -0400

    initial commit

---
+++ app/src/main/java/com/alexjlockwood/example/playpauseanimation/MainActivity.java
@@ -0,0 +1,22 @@
+package com.alexjlockwood.example.playpauseanimation;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final PlayPauseView view = (PlayPauseView) findViewById(R.id.play_pause_view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                view.toggle();
+            }
+        });
+    }
+}

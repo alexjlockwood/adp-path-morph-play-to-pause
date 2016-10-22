3aaa80bc3efea8c6da65fb073438e52d55756d34
Alex Lockwood <alexjlockwood@gmail.com>
2016-10-22 14:35:53 -0400

    animated vector drawable implementation

---
+++ app/src/main/java/com/alexjlockwood/example/playpauseanimation/CheckableFloatingActionButton.java
@@ -0,0 +1,51 @@
+package com.alexjlockwood.example.playpauseanimation;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+
+public class CheckableFloatingActionButton extends FloatingActionButton implements Checkable {
+  private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
+
+  private boolean isChecked;
+
+  public CheckableFloatingActionButton(Context context) {
+    this(context, null);
+  }
+
+  public CheckableFloatingActionButton(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public CheckableFloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  public boolean isChecked() {
+    return isChecked;
+  }
+
+  @Override
+  public void setChecked(boolean isChecked) {
+    if (isChecked != this.isChecked) {
+      this.isChecked = isChecked;
+      refreshDrawableState();
+    }
+  }
+
+  @Override
+  public void toggle() {
+    setChecked(!isChecked);
+  }
+
+  @Override
+  public int[] onCreateDrawableState(int extraSpace) {
+    final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+    if (isChecked()) {
+      mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+    }
+    return drawableState;
+  }
+}
--- app/src/main/java/com/alexjlockwood/example/playpauseanimation/MainActivity.java
+++ app/src/main/java/com/alexjlockwood/example/playpauseanimation/MainActivity.java
@@ -1,22 +1,29 @@
 package com.alexjlockwood.example.playpauseanimation;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final PlayPauseView view = (PlayPauseView) findViewById(R.id.play_pause_view);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                view.toggle();
-            }
-        });
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    final CheckableFloatingActionButton fab =
+        (CheckableFloatingActionButton) findViewById(R.id.checkable_fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        fab.toggle();
+      }
+    });
+    final PlayPauseView ppv = (PlayPauseView) findViewById(R.id.play_pause_view);
+    ppv.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        ppv.toggle();
+      }
+    });
+  }
 }
